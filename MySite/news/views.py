13a412bd0b08d9360873067d847b71ec16a0bf2d
BloodyDoc21 <sven-60@yandex.ru>
2026-05-04 18:08:25 +0300

    10 лаба, вариант 1

---
+++ MySite/news/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
+from .models import News, Category
+
+def index(request):
+    news = News.objects.order_by('-created_at')
+    context = {
+        'news': news,
+        'title': 'Список новостей'
+    }
+    
+    return render(request, template_name='news/index.html', context=context)
+
+def get_category(request, category_id):
+    news = News.objects.filter(category_id=category_id).order_by('-created_at')
+    category = get_object_or_404(Category, pk=category_id)
+    context = {
+        'category': category,
+        'news': news
+    }
+    return render(request, 'news/category.html', context)
+
+def test(request):
+    return HttpResponse('<h1>Тестовая страница<h1>')
+
+def view_news(request, news_id):
+    news_item = get_object_or_404(News, pk=news_id)
+    return render(request, 'news/view_news.html', {"news_item": news_item})
